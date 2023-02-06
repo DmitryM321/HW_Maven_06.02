f82cea48fa194fe8ca81dede7e2525ac7e7383e6
Dmitry <a89297175356@gmail.com>
2023-02-06 16:48:46 +0500

    ДЗ1

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+        final String user = "postgres";
+        final String password = "8363";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+    }
+}
